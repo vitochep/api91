0f2c357b86257196f12c9175560d799e79f2a2a1
Vitaliy <animus2393@gmail.com>
2020-06-23 16:13:37 +0300

    fix upload controller

--- controllers/upload.js
+++ controllers/upload.js
@@ -4,20 +4,19 @@ const {
     Dialog: DialogModel,
 } = require('../models');
 
+const { dialogOne: dialogOneResponse } = require('../responses');
+
 module.exports = async (req, res) => {
     const {avatar} = req.files;
 
     let dialogAvatar = avatar.md5;
     console.log(dialogAvatar);
 
-    fs.rename(avatar.tempFilePath, process.env.TEMP_FILE_DIR + '/' + dialogAvatar, (err) => {
+    fs.rename(avatar.tempFilePath, process.env.TEMP_FILE_DIR + '/' + dialogAvatar + '.jpg', (err) => {
         console.log('err', err);
     });
 
-    console.log(req);
-
     const dialogId = req.body.dialogId;
-
 
     const dialog = await DialogModel.findOne({
         where: {
@@ -25,6 +24,9 @@ module.exports = async (req, res) => {
         }
     });
 
-    res.json({test: true});
-};
+    console.log(dialog)
 
+    dialog.update({avatar:dialogAvatar + '.jpg'});
+
+    res.json(dialogOneResponse(dialog));
+};
--- models/dialog.js
+++ models/dialog.js
@@ -1,7 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
 	const Dialog = sequelize.define('Dialog', {
-		name: DataTypes.STRING
+		name: DataTypes.STRING,
+		avatar: DataTypes.STRING,
 	}, {});
 	Dialog.associate = function(models) {
 		// 
